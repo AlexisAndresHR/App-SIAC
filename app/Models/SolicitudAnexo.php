aa0d255c92fecdc821ac4c905aab5e98f2cab157
AlexisAndresHR <1717110253@utectulancingo.edu.mx>
2021-10-18 22:00:25 -0500

    App Models generated (based on DB tables)

---
+++ app/Models/SolicitudAnexo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SolicitudAnexo extends Model
+{
+    use HasFactory;
+    protected $table = 'solicitud_anexos';
+}
